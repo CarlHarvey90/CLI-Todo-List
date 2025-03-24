0654c064f9b03e01bc77bff182346c600836dcf5
Carl Harvey <c.harvey@hotmail.co.za>
2025-03-24 11:00:46 +0000

    Update main.py
    Added menu items

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import sys
+menu = ("Select one of the following options: \n"
+  "1. Add another item \n"
+  "2. Remove an item \n"
+  "3. View item list \n"
+  "4. Home \n")
 
-def add():
-  print("I am add")
+def add(user_input):
+  print("New item added " + user_input)
+  print(menu)
+  #print("Select one of the following options: ")
+  #print("1. Add another item")
+  #print("2. Remove an item")
+  #print("3. View item list")
+  #print("4. Home")
+  
 
 def main():
-  
+  welcome = ("Welcome to the Todo list app. Please choose one of the following options: \n"
+  "1. Add an item \n"
+  "2. Remove and item \n"
+  "3. View item list \n"
+  "4. Exit")
+  print(welcome)
+  #print(menu)
   while True:
-    user_input = input("Please enter something: ")
-  
-    if user_input.lower() == "exit":
+    
+    user_input = input()
+    
+    if user_input.lower() == "exit" or "4":
       print("Exiting Todo List")
       break
     
-    if user_input == 'add':
-      add()
+    if user_input == '1':
+      print("Type in a item to add to the list: ")
+      new_item = input()
+      add(new_item)
 
-    print(user_input)
+    #print(user_input)
 if __name__ == '__main__':
   main()
