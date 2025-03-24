72b630c2052adcf336875aa4d32f938b5bd78c91
Carl Harvey <c.harvey@hotmail.co.za>
2025-03-24 09:48:37 +0000

    Update main.py
    Added some test code for user input and keeping the app open until the user exits

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def main():
-  print("Hello")
+import sys
 
+def add():
+  print("I am add")
+
+def main():
+  
+  while True:
+    user_input = input("Please enter something: ")
+  
+    if user_input.lower() == "exit":
+      print("Exiting Todo List")
+      break
+    
+    if user_input == 'add':
+      add()
+
+    print(user_input)
 if __name__ == '__main__':
   main()
